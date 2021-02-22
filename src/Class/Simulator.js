76939f80fbd7b320a17a888d69fda026be51acbf
Aloïs <zelvekalois@outlook.com>
2021-02-22 21:21:10 +0100

    Fix Linter

--- src/Class/Simulator.js
+++ src/Class/Simulator.js
@@ -6,6 +6,7 @@ let Simulator = class extends Endpoints {
     request;
     db;
     uuid;
+
     constructor() {
         super();
         this.request = require('request');
@@ -27,15 +28,14 @@ let Simulator = class extends Endpoints {
 
         let data = this;
 
-       let interval = setInterval(function(){
-           console.log("lol")
-           data.request( data.endpointBinance + "/api/v3/klines?symbol=" + symbol + "&interval=1m&limit=100", { json: true }, (err, res, body) => {
+        let interval = setInterval(function () {
+            data.request(data.endpointBinance + "/api/v3/klines?symbol=" + symbol + "&interval=1m&limit=100", {json: true}, (err, res, body) => {
 
-               data.db.getSimulatorDb().get('Simulation')
-                   .find({ uuid: uuid })
-                   .assign({final : body[body.length - 1 ][4]})
-                   .write()
-           });
+                data.db.getSimulatorDb().get('Simulation')
+                    .find({uuid: uuid})
+                    .assign({final: body[body.length - 1][4]})
+                    .write()
+            });
 
             clearInterval(interval);
         }, ms(frequency) * 3)
